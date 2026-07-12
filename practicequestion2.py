a54c5f657f0163c6def5938b622ee7aea1be2284
Madhuja Tathe <madhutathe38@gmail.com>
2026-07-13 00:07:06 +0530

    Update practicequestion2.py

--- practicequestion2.py
+++ practicequestion2.py
@@ -22,4 +22,36 @@ else:
     grade = "D"
 print("grade of the student ->", grade)
 
+#4.
+#wap to check if a number entered by the user is odd or even
+num = int(input("enter a number"))
+
+rem = num % 2
+if(rem == 0):
+    print("EVEN")
+else:
+    print("ODD")
+
+#5.
+#wap to find the greatest of 3 number entered by the user
+a = int(input("enter a  first number"))
+b = int(input("enter a  second number"))
+c = int(input("enter a   third number"))
+
+if(a >= b and a>= c):
+    print("first number is largest",a)
+elif(b >= c):
+    print("second number is largest",b)
+else:
+    print("third is largest",c)
+    
+
+#6.
+#wap to check if a number is a multiple of 7 or not
+x = int(input("enter a number"))
+
+if(x % 7 ==0):
+    print("multiple of 7")
+else:
+    print("not a multiple")
 
