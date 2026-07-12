16bb1ae1dc9035f134f84f0bdc35a674b8f5fd85
Madhuja Tathe <madhutathe38@gmail.com>
2026-07-12 23:41:57 +0530

    Update practicequestion2.py

--- practicequestion2.py
+++ practicequestion2.py
@@ -8,5 +8,18 @@ print(len(name))
 str1 = "i have 10$ to 5$ bill"
 print(str1.count("$"))
 
+#3.
+#grade student based on marks
+markes = int(input("enter student marks: "))
+
+if(markes >=90):
+    grade = "A"
+elif(marks >=80 and markes <90):
+    grade = "B"
+elif(marks >=70 and marks < 80):
+    grade = "C"
+else:
+    grade = "D"
+print("grade of the student ->", grade)
 
 
