9da927e4302fe2062aa3b1f56da80ccce15080c5
Madhuja Tathe <madhutathe38@gmail.com>
2026-07-14 23:23:29 +0530

    Create listandtuplelesson3.py

---
+++ listandtuplelesson3.py
@@ -0,0 +1,12 @@
+#LIST
+
+#a built-in data type that stores set of values
+#it can store elements if different types(integer,float,string,etc)
+
+#marks1 = 94.4  writting marks of every student by creating new variable and handling is also hard 
+#marks2 = 87.3   therefore there is a built-in data type called list
+#marks3 = 95.5
+#marks4 = 66.9
+marks = [94.4, 87.3, 95.5, 66.9]  #its a list 
+#its always in square braket and seperated by ','.
+print(type(marks))   #we can print type of a list
