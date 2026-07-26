39936c2770fec47bc930e867b51aa3d2e12a2fc8
Madhuja Tathe <madhutathe38@gmail.com>
2026-07-26 22:09:48 +0530

    Update listandtuplelesson3.py

--- listandtuplelesson3.py
+++ listandtuplelesson3.py
@@ -1,4 +1,4 @@
-#LIST
+#LIST     [basic, slicing, methods]
 
 #a built-in data type that stores set of values
 #it can store elements if different types together(integer,float,string,etc)
@@ -46,7 +46,26 @@ print(marks[1:4])   #first three values will be printed
 #in list slicing we also have negative indexing like string
 print(marks[-3:-1]) 
 
-#list method
+#LIST METHODS  [functions]
+list = [2, 1, 3]
+
+#basic function of list
+#1.
+list.append(4)            # adds one element at the end   [2, 1, 3, 4]
+
+#2.
+list.sort()               #sorts in  ascending order [1, 2, 3]
+
+#3.
+list.sort(reverse=true)   #sorts in descending order [3, 2, 1]
+
+#4.
+list.reverse()            # reverse list  [3, 1, 2]
+
+#5. 
+list.inster(idx,el)       #insert element at index   
+
+ 
 
 
 
